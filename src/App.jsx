d3921fb3072baffda56c711b138886983789677c
Mysteray01 <rayartmasbate@gmail.com>
2026-09-04 16:59:50 +0800

    html replica

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+export default function App() {
+  return (
+    <>
+      <head></head>
+      <body class="font-['Times_New_Roman',serif] ml-2 mr-2">
+
+        <h1 class="text-3xl font-semibold mt-6">Ray Art S. Masbate</h1>
+        <p class="text-base mt-4.5">Third year BSIT student at Cebu Institute of Technology – University.</p>
+        <hr class="mt-3 opacity-40"/>
+
+        <div>
+          <h2 class="text-2xl font-semibold mt-4 mb-4">About</h2>
+          <p class="leading-4.5">
+            I am a student at CIT-U with a strong interest in software development.
+            I like to explore different programming languages and creating games.
+          </p>
+        </div>
+
+        <div>
+          <h2 class="text-2xl font-semibold mt-4 mb-4">Details</h2>
+          <p class="leading-4.5">
+            Course: BS Information Technology
+            <br />
+            Year level: Third year
+            <br />
+            Hometown: Argawanon, San Remigio, Cebu
+          </p>
+        </div>
+
+        <div>
+          <h2 class="text-2xl font-semibold mt-4 mb-4">Things I like</h2>
+          <ul class="list-disc pl-10 space-y-1 leading-3.5">
+            <li>I like playing gacha games</li>
+            <li>I love playing badminton</li>
+            <li>Sinigang lover</li>
+          </ul>
+        </div>
+
+        <div>
+          <h2 class="text-2xl font-semibold mt-4 mb-4">Reach me</h2>
+          <p class="leading-4.5">
+            rayart.masbate@cit.edu
+            <br/>
+            @rayartmasbate on most things
+          </p>
+        </div>
+
+        <div>
+          <hr class="mt-3 mb-3 opacity-40"/>
+          <p>Made for CSIT340.</p>
+        </div>
+
+      </body>
+    </>
+  );
+}
